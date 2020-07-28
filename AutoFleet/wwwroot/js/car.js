88acd973956dd0bd4d44b4cf2bca8c38f8715196
Adi Roman <adiroman96@gmail.com>
2020-07-28 22:28:25 +0300

    EditCar refactored

--- AutoFleet/wwwroot/js/car.js
+++ AutoFleet/wwwroot/js/car.js
@@ -1,20 +1,21 @@
 ﻿const registrationNumberInput = document.getElementById("registrationNumberInput");
 const manufacturingYearInput = document.getElementById("manufacturingYearInput");
-const driverNameInput = document.getElementById("driverNameInput");
 const carIdInput = document.getElementById("Id");
 const driverDropdown = document.getElementById("changeDriverDropdown");
 const lastITPInput = document.getElementById("lastITPInput");
 const expirationDateITPInput = document.getElementById("expirationDateITPInput");
-const defaultOptionForDriverDropdown = 'Alege noul sofer';
 
 $(document).ready(function () {
-    fillFields();
-    getDrivers(); // populate select with all drivers
+
+    var currentURL = window.location.href;
+    var idCar = currentURL.split("/Cars/Edit/")[1]; // get id of current car
+
+    fillFields(idCar); // fill known fields
 });
 
 
-function fillFields() {
-    fetch('/cars/cardto/' + carId)
+function fillFields(idCar) {
+    fetch('/cars/cardto/' + idCar)
         .then(
             function (response) {
                 if (response.status !== 200) {// ToDo: make it give a nice error on the page
@@ -27,7 +28,7 @@ function fillFields() {
                 response.json().then(function (data) {
                     registrationNumberInput.value = data.carRegistrationNumber;
                     manufacturingYearInput.value = data.carManufacturingYear;
-                    driverNameInput.value = data.driverName;
+                    prepareDriverDropdown(data.idDriver, data.driverName, data.driverEmail);
                 });
             }
         )
@@ -36,12 +37,18 @@ function fillFields() {
         });
 }
 
-const getDrivers = async () => {
+/*
+ * Sets main option to "driverName"
+ * and loads other drivers
+ */
+function prepareDriverDropdown(idDriver, driverName, driverEmail) {
 
     driverDropdown.length = 0;
 
     let defaultOption = document.createElement('option');
-    defaultOption.text = defaultOptionForDriverDropdown;
+    defaultOption.text = driverName;
+    defaultOption.title = driverName + " " + driverEmail;
+    defaultOption.value = idDriver;
 
     driverDropdown.add(defaultOption);
     driverDropdown.selectedIndex = 0;
@@ -60,44 +67,15 @@ const getDrivers = async () => {
                     const data = myJson.data; // get data from the json
 
                     for (let i = 0; i < data.length; i++) {
-                        option = document.createElement('option');
-                        option.title = data[i].name + " " + data[i].email;
-                        option.text = data[i].name;
-                        option.value = data[i].id;
-                        driverDropdown.add(option);
+
+                        if (data[i].id !== idDriver) {
+                            option = document.createElement('option');
+                            option.title = data[i].name + " " + data[i].email;
+                            option.text = data[i].name;
+                            option.value = data[i].id;
+                            driverDropdown.add(option);
+                        }
                     }
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err); // ToDo: make it give a nice error on the page
-        });
-}
-
-driverDropdown.onchange = function () {
-    setDriver(driverDropdown.value);
-}
-
-const setDriver = async (id) => {
-    if (id == defaultOptionForDriverDropdown)
-        return;
-
-    fetch('/drivers/getdriver/' + id)
-        .then(
-            function (response) {
-                if (response.status !== 200) {// ToDo: make it give a nice error on the page
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                // Examine the text in the response
-                response.json().then(function (driver) {
-                    console.log(driver.name);
-                    driverDropdown.title = "asd";
-
-                    let driverName = document.getElementById("driverNameInput");
-                    driverName.value = driver.name;
                 });
             }
         )
