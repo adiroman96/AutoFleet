a21038ded824f48ca59ef6b20823dc76e4e4eae7
Adi Roman <adiroman96@gmail.com>
2020-08-10 07:47:55 +0300

    UI Romana

--- AutoFleet/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AutoFleet/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace AutoFleet.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Tine-ma minte?")]
             public bool RememberMe { get; set; }
         }
 
